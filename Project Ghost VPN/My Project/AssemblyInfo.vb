1daaa5be84e31e7251927d7e947a325a3c4a7760
Ven <rein.akihisa@gmail.com>
2022-01-17 22:47:01 +0800

    Resources

---
+++ Project Ghost VPN/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Project Ghost VPN")>
+<Assembly: AssemblyDescription("Desktop GUI application of VPN and mainly a modern Version of Cyber VPN")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Project Ghost VPN")>
+<Assembly: AssemblyCopyright("Copyright ©  2017 Akihisa Ren. All rights reserved.")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(True)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f7f1b6e0-4a13-49d4-8f4b-e50f6be166cc")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.3")>
+<Assembly: AssemblyFileVersion("1.0.0.3")>
